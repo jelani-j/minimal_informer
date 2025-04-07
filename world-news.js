1840a75afc15d54ab161879282789556a474c2d4
jelani_j <jelnjoseph4@gmail.com>
2025-04-06 23:34:22 -0400

    attempting to fix axios error in browser

--- world-news.js
+++ world-news.js
@@ -1,10 +1,10 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+import axios from 'axios';
+import * as cheerio from 'cheerio';
 const url_test = 'https://www.axios.com/energy-climate/extreme-weather';
 
 // when you use .then it reacts based of the response of the previous statement, quite literarrly an "if statement"
 // extract "headline"
-function world_climate_headlines(){
+function world_climate_headlines(url_test){
   axios.get(url_test).then(response =>{
     // returns entirety of page's html contents as a large "string"
     const html = response.data;
@@ -21,22 +21,22 @@ function world_climate_headlines(){
   })
 }
 
-world_climate_headlines();
+world_climate_headlines(url_test);
 
-function tech_news_headlines(){
-  const url = 'https://brutalist.report/topic/tech?'
-  const parser = require('cheerio')
-  axios.get(url).then(response =>{
-    const html = response.data;
-    const headlines = [];
-    const doc = parser.load(html);
-    doc('li').each((index, element) => {
-      headlines.push(doc(element).text().trim('\n +'));
-    });
-    console.log(headlines);
-  })
+// function tech_news_headlines(){
+//   const url = 'https://brutalist.report/topic/tech?'
+//   const parser = require('cheerio')
+//   axios.get(url).then(response =>{
+//     const html = response.data;
+//     const headlines = [];
+//     const doc = parser.load(html);
+//     doc('li').each((index, element) => {
+//       headlines.push(doc(element).text().trim('\n +'));
+//     });
+//     console.log(headlines);
+//   })
   
-}
+// }
 
-tech_news_headlines();
+// tech_news_headlines();
 
