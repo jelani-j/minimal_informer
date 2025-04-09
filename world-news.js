1bb78dd81bb83238a80ddc4fe53d8da76faddf5d
jelani_j <jelnjoseph4@gmail.com>
2025-04-08 21:09:38 -0400

    finally able to assume role, now need to grab parameter and attempt API call

--- world-news.js
+++ world-news.js
@@ -1,42 +1,73 @@
-import axios from 'axios';
-import * as cheerio from 'cheerio';
-const url_test = 'https://www.axios.com/energy-climate/extreme-weather';
+import { STSClient, AssumeRoleCommand } from "@aws-sdk/client-sts";
+import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
+import {fromIni} from "@aws-sdk/credential-provider-ini";
 
-// when you use .then it reacts based of the response of the previous statement, quite literarrly an "if statement"
-// extract "headline"
-function world_climate_headlines(url_test){
-  axios.get(url_test).then(response =>{
-    // returns entirety of page's html contents as a large "string"
-    const html = response.data;
-    const headlines = [];
-    // use cherio import to load aka transfer the response data to easily manipulatable tempalte
-    const cheerio_parse = cheerio.load(html);
-    // look for "headline"
-    cheerio_parse('h3').each((index, element) => {
-      headlines.push(cheerio_parse(element).text());
-    });
-    console.log(headlines);
-    return{headlines}
-    
-  })
+const stsClient = new STSClient({ region: "us-east-2"});
+
+const command = new AssumeRoleCommand({
+  RoleArn: "arn:aws:iam::203662895152:role/java-sdk-role",
+  RoleSessionName: "JellyDevTestSession"
+});
+
+try{
+  const response = await stsClient.send(command);
+  console.log("Assume Role Success!",
+    response.Credentials.AccessKeyId);
+  console.log("Session Expired", response.Credentials.Expiration);
+
+} catch(err){
+  console.error("AssumeRole Failed:");
+  console.error(err);
+
 }
 
-world_climate_headlines(url_test);
 
-// function tech_news_headlines(){
-//   const url = 'https://brutalist.report/topic/tech?'
-//   const parser = require('cheerio')
-//   axios.get(url).then(response =>{
-//     const html = response.data;
-//     const headlines = [];
-//     const doc = parser.load(html);
-//     doc('li').each((index, element) => {
-//       headlines.push(doc(element).text().trim('\n +'));
-//     });
-//     console.log(headlines);
+
+
+// async function assumeRoleAndExecuteSSMCommand() {
+//   try {
+//       // Assume the role
+//       const assumeRoleCommand = new AssumeRoleCommand(assumeRoleParams);
+//       const assumeRoleResponse = await STSClient.send(assumeRoleCommand);
+
+//       // Configure SSM client with assumed role credentials
+//       const ssmClient = new SSMClient({
+//           region: "us-east-2",
+//           credentials: {
+//               accessKeyId: assumeRoleResponse.Credentials.AccessKeyId,
+//               secretAccessKey: assumeRoleResponse.Credentials.SecretAccessKey,
+//               sessionToken: assumeRoleResponse.Credentials.SessionToken,
+//           },
+//       });
+//     }catch (error) {
+//       console.error("Error:", error);
+//     }
+//   }
+
+//   assumeRoleAndExecuteSSMCommand();
+
+// const getParameterValue = async (parameterArn) => {
+//   const command = new GetParameterCommand({
+//     Name: parameterArn,
+//     WithDecryption: true,
+//   });
+
+//   try {
+//     const response = await ssmClient.send(command);
+//     return response.Parameter.Value;
+//   } catch (error) {
+//     console.error("Error retrieving parameter:", error);
+//     throw error;
+//   }
+// };
+
+// const parameterArn = 'arn:aws:ssm:us-east-2:203662895152:parameter/gnews-api';
+
+// getParameterValue(parameterArn)
+//   .then((value) => {
+//     console.log("Parameter value:", value);
 //   })
-  
-// }
-
-// tech_news_headlines();
+//   .catch((error) => {
+//     // Handle the error appropriately
+//   });
 
