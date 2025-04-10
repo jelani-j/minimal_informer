3b112c175f8788ea94bae89a3353110b355c0c2d
jelani_j <jelnjoseph4@gmail.com>
2025-04-09 20:47:31 -0400

    able to now grab parameter from store

--- world-news.js
+++ world-news.js
@@ -1,26 +1,42 @@
 import { STSClient, AssumeRoleCommand } from "@aws-sdk/client-sts";
-import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
+import { SSMClient, GetParameterCommand} from "@aws-sdk/client-ssm";
 import {fromIni} from "@aws-sdk/credential-provider-ini";
+const arn = "arn:aws:iam::203662895152:role/java-sdk-role";
+const parameterArn = 'arn:aws:ssm:us-east-2:203662895152:parameter/gnews-api';
+var api_key = "";
 
 const stsClient = new STSClient({ region: "us-east-2"});
 
-const command = new AssumeRoleCommand({
-  RoleArn: "arn:aws:iam::203662895152:role/java-sdk-role",
-  RoleSessionName: "JellyDevTestSession"
-});
-
-try{
-  const response = await stsClient.send(command);
-  console.log("Assume Role Success!",
-    response.Credentials.AccessKeyId);
-  console.log("Session Expired", response.Credentials.Expiration);
-
-} catch(err){
-  console.error("AssumeRole Failed:");
-  console.error(err);
-
+async function assume_role(){
+  const command = new AssumeRoleCommand({
+    RoleArn: arn,
+    RoleSessionName: "JellyDevTestSession"
+  });
+  //assume proper role and establish ssm Client
+  try{
+    const assumed_role = await stsClient.send(command);
+    //Configure SSM Client with Assumed Role 
+    const ssmClient = new SSMClient({
+      region: "us-east-2",
+      credentials: {
+        accessKeyId: assumed_role.Credentials.AccessKeyId,
+        secretAccessKey: assumed_role.Credentials.SecretAccessKey,
+        sessionToken: assumed_role.Credentials.SessionToken
+      }
+    });
+    const getParameterCommand = new GetParameterCommand({
+      Name: "gnews-api",
+      WithDecryption: true,
+    });
+    const response = await ssmClient.send(getParameterCommand);
+    console.log("Paramater Response:", response.Parameter.Value);
+  } catch(err){
+    console.error("AssumeRole Failed:");
+    console.error(err);
+  }
 }
 
+assume_role();
 
 
 
