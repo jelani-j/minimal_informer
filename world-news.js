b026f56eccea4a5aeb95c8fd0f9957d237a3ace1
jelani_j <jelnjoseph4@gmail.com>
2025-04-11 19:04:42 -0400

    figured out query serach, need to replicate this then write to json file

--- world-news.js
+++ world-news.js
@@ -3,9 +3,11 @@ import { SSMClient, GetParameterCommand} from "@aws-sdk/client-ssm";
 import {fromIni} from "@aws-sdk/credential-provider-ini";
 const arn = "arn:aws:iam::203662895152:role/java-sdk-role";
 const stsClient = new STSClient({ region: "us-east-2"});
+// dictionaries for different news sources
+let world_news_dict = []; // all world news dicts will be stored in here
+let climate_dict = {};
 
-let world_news_dict = {};
-
+//when using await for api pull, you must also await it 
 
 async function assume_role(){
   const command = new AssumeRoleCommand({
@@ -35,26 +37,29 @@ async function assume_role(){
     console.error(err);
   }
 }
-
+//Enviromental Disaster
 async function world_news_enviroment(){
   const api_key = await assume_role();
-  const url = 'https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=' + api_key;
-  let climate_dict = {};
+  const query = 'environmental disaster OR earthquake OR flood OR wildfire OR hurricane OR drought OR landslide OR typhoon';
+  const encodedQuery = encodeURIComponent(query);
+  const url = 'https://gnews.io/api/v4/search?q=' + encodedQuery + '&lang=en&token=' + api_key;
   fetch(url)
       .then(response => response.json())
       .then(data => {
         const articles = data.articles;
         articles.forEach(article => {
           climate_dict[article.title] = article.description;
-          // console.log('Title:', article.title);
-          // console.log('Description:', article.description);
         });
-        console.log(Object.keys(climate_dict));
-        // console.log(Object.values(climate_dict));
+        world_news_dict.push(climate_dict);
       })
       .catch(error => console.error('Error:', error));
-}
-
+ }
 
 world_news_enviroment();
+
+// async function world_news_global(){
+//   const api_key = await assume_role();
+//   const url = 'https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=' + api_key;
+
+// }
 
