9763efa9e9cfd9fdbd30d202b0b0704d3578478a
jelani_j <jelnjoseph4@gmail.com>
2025-04-12 20:15:42 -0400

    got data populating for world-news

--- world-news.js
+++ world-news.js
@@ -1,11 +1,15 @@
 import { STSClient, AssumeRoleCommand } from "@aws-sdk/client-sts";
 import { SSMClient, GetParameterCommand} from "@aws-sdk/client-ssm";
 import {fromIni} from "@aws-sdk/credential-provider-ini";
+import * as fs from 'fs';
 const arn = "arn:aws:iam::203662895152:role/java-sdk-role";
 const stsClient = new STSClient({ region: "us-east-2"});
 // dictionaries for different news sources
-let world_news_dict = []; // all world news dicts will be stored in here
+const world_news_dict = []; // all world news dicts will be stored in here
 let climate_dict = {};
+let global_dict = {};
+let local_dict = {};
+const filePath = "api_data.json";
 
 //when using await for api pull, you must also await it 
 
@@ -41,10 +45,10 @@ async function assume_role(){
 //Enviromental Disaster
 async function world_news_enviroment(){
   const api_key = await assume_role();
-  const query = 'environmental disaster OR earthquake OR flood OR wildfire OR hurricane OR drought OR landslide OR typhoon';
+  const query = 'environmental disaster OR earthquake OR flood OR wildfire OR hurricane OR drought OR landslide OR typhoon AND NOT "Trump"';
   const encodedQuery = encodeURIComponent(query);
   const url = 'https://gnews.io/api/v4/search?q=' + encodedQuery + '&lang=en&max=10&token=' + api_key;
-  fetch(url)
+  await fetch(url)
       .then(response => response.json())
       .then(data => {
         const articles = data.articles;
@@ -56,10 +60,58 @@ async function world_news_enviroment(){
       .catch(error => console.error('Error:', error));
  }
 
+// global news
+async function world_news_global(){
+  const api_key = await assume_role();
+  const query = 'world';
+  const encodedQuery = encodeURIComponent(query);
+  const url = 'https://gnews.io/api/v4/top-headlines?category=' + encodedQuery + '&lang=en&max=10&apikey=' + api_key;
+  await fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const articles = data.articles;
+        articles.forEach(article => {
+          global_dict[article.title] = article.description;
+        });
+        world_news_dict.push(global_dict);
+      })
+      .catch(error => console.error('Error:', error));
+}
 
-// async function world_news_global(){
-//   const api_key = await assume_role();
-//   const url = 'https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=' + api_key;
+//local news
+async function local_news(){
+  const api_key = await assume_role();
+  const query = 'manchester connecticut';
+  const encodedQuery = encodeURIComponent(query);
+  const url = 'https://gnews.io/api/v4/search?q=' + encodedQuery + '&lang=en&max=10&token=' + api_key;
+  await fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const articles = data.articles;
+        articles.forEach(article => {
+          local_dict[article.title] = article.description;
+        });
+        world_news_dict.push(local_dict);
+      })
+      .catch(error => console.error('Error:', error));
+}
 
-// }
 
+function writeArrayOfDictToJson(filePath, array) {
+  const jsonString = JSON.stringify(array, null, 2); // Convert array to JSON string with indentation
+  fs.writeFile(filePath, jsonString, (err) => {
+    if (err) {
+      console.error("Error writing to file:", err);
+    } else {
+      console.log("Successfully wrote data to file:", filePath);
+    }
+  });
+}
+
+async function savedata(){
+  await Promise.all([world_news_enviroment(), world_news_global(), local_news()]);
+  writeArrayOfDictToJson(filePath, world_news_dict);
+}
+
+// savedata().then(result =>{ console.log(result)});
+savedata().then(data => {console.log(data)});
