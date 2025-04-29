2386947d9ad507af6147f26a8d93c6b6e47102b7
jelani_j <jelnjoseph4@gmail.com>
2025-04-28 22:24:40 -0400

    testing once more

--- functions/world-news.js
+++ functions/world-news.js
@@ -77,6 +77,7 @@ exports.handler = async (event) => {
   // Your Lambda code logic here
   // this is a test to see if you can see me or not
   // lets see if this one works!
+  //final test, last push was successful if all else fail then go back
   return {
       statusCode: 200,
       body: JSON.stringify('Hello from Lambda!'),
