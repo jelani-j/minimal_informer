045675a1ce78b5a2157bcc391d3b18b1e9a80005
jelani_j <jelnjoseph4@gmail.com>
2025-05-04 00:02:32 -0400

    updates to allow for cors header

--- functions/dynamodb_pull.js
+++ functions/dynamodb_pull.js
@@ -29,7 +29,9 @@ exports.handler = async (event) => {
     return {
       statusCode: 200,
       headers: {
-        "Access-Control-Allow-Origin": "*"
+        "Access-Control-Allow-Origin": "*",
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET,OPTIONS',
       },
       body: JSON.stringify(articles)
     };
